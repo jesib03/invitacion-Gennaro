0468df0912cf9c2b97485dbc80d5779fa8d5a3dd
Jesica Brito <jesib03@hotmail.com>
2026-05-20 16:48:32 -0300

    home invitacion

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import InvitationCard from "@/components/InvitationCard";
 
-export default function InvitacionPage() {
+export default function HomePage() {
   return (
     <main
-  className="
-   min-h-screen
+      className="
+        min-h-screen
     flex
     items-start
     justify-center
@@ -12,9 +12,9 @@ export default function InvitacionPage() {
     pb-10
     px-4
     overflow-hidden
-  "
->
-  <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-transparent pointer-events-none" />
+      "
+    >
+      <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-transparent pointer-events-none" />
       <InvitationCard />
     </main>
   );
